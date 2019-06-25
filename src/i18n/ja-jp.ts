2b74eb118aa82d7f647d3ca34d271d0e4a3aab33
Girai Kaku <kakugirai@gmail.com>
2019-06-26 08:23:24 +0900

    add japanese support (#2)

---
+++ src/i18n/ja-jp.ts
@@ -0,0 +1,13 @@
+export default {
+  assignments: '課題',
+  cnsPassword: 'CNS パスワード',
+  cnsUsername: 'CNS ユーザー名',
+  forgetPassword: 'パスワードを忘れた場合',
+  goToSfs: 'SFC-SFS へ',
+  goToWellness: 'Wellness ヘ',
+  login: 'ログイン',
+  logOut: 'ログアウト',
+  passwordSecurity: 'この拡張機能は、ユーザー名・パスワードをローカルに保存してます。第三者にデータを提供しません。',
+  rememberPassword: 'Remember',
+  timetable: '時間割',
+};
