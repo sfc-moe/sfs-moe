130e99969e81bad042e61c8deb1483655421bb16
Shaimay Shah <shaimay.pathways@gmail.com>
2019-07-16 12:25:32 +0900

    Add Hindi support (#3)

---
+++ src/i18n/in-hi.ts
@@ -0,0 +1,19 @@
+export default {
+  assignments: 'असाइनमेट'
+  cnsPassword: 'CNS का पासवर्ड',
+  cnsUsername: 'CNS का उपयोगकर्ता नाम',
+  finished: 'समाप्त',
+  forgetPassword: 'पासवर्ड भूल गए',
+  goToSfs: 'SFC-SFS को खोलो',
+  goToWellness: 'Wellness के वेबसाइट को खोलो',
+  loading: 'मोड़ हो रहा हैं',
+  login: 'लॉग इन करें',
+  loginFailed: 'लॉगिन विफल। कृपया अपना उपयोगकर्ता नाम या पासवर्ड जांचें।',
+  logOut: 'लोग आउट',
+  missed: 'छूटे हुए असाइनमेंट',
+  passwordSecurity: 'CNS उपयोगकर्ता नाम और पासवर्ड केवल प्लगइन में रखे जाएंगे। हम इसे किसी तीसरे पक्ष को नहीं भेजेंगे।',
+  upcoming: 'आने वाले असाइनमेंट',
+  rememberPassword: 'स्वतः लॉगइन',
+  timetable: 'समय सारणी',
+};
+
